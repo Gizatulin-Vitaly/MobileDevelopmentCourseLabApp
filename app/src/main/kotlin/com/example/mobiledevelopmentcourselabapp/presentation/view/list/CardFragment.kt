b8fb379717902b6583b5774fc009cddd8bce3e34
Gizatulin-Vitaly <my@vgmail.ru>
2024-04-01 22:04:05 +0500

    Task 4 - Комментарий

--- app/src/main/kotlin/com/example/mobiledevelopmentcourselabapp/presentation/view/list/CardFragment.kt
+++ app/src/main/kotlin/com/example/mobiledevelopmentcourselabapp/presentation/view/list/CardFragment.kt
@@ -2,6 +2,7 @@ package com.example.mobiledevelopmentcourselabapp.presentation.view.list
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
@@ -9,11 +10,16 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.Group
+import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.mobiledevelopmentcourselabapp.R
 import com.example.mobiledevelopmentcourselabapp.databinding.FragmentCardBinding
 import com.example.mobiledevelopmentcourselabapp.presentation.view.list.model.CardUiModel
+import com.example.mobiledevelopmentcourselabapp.utils.orFalse
+import com.google.android.material.snackbar.Snackbar
 import org.jsoup.select.Evaluator.IsNthChild
 
 class CardFragment : Fragment() {
@@ -59,6 +65,15 @@ class CardFragment : Fragment() {
         startActivity(shareIntent)
     }
 
+    private fun visibleHiddenGroup() {
+        binding.comments.hiddenGroup.isVisible = binding.comments.hiddenGroup.isVisible.not()
+        if (binding.comments.hiddenGroup.isVisible) {
+            binding.comments.chevron.rotation = DEGREE_OF_ROTATION
+        } else {
+            binding.comments.chevron.rotation = NOT_ROTATION
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
@@ -81,6 +96,30 @@ class CardFragment : Fragment() {
             binding.fan.text = card.formattedfan
         }
 
+        binding.comments.sendButton.isEnabled = false
+
+
+
+        binding.comments.commentTitle.setOnClickListener {
+            visibleHiddenGroup()
+        }
+        binding.comments.chevron.setOnClickListener {
+            visibleHiddenGroup()
+        }
+
+        binding.comments.commentInput.doOnTextChanged { text, _, _, _ ->
+            binding.comments.sendButton.isEnabled = text?.isNotBlank().orFalse()
+        }
+
+        binding.comments.sendButton.setOnClickListener {
+            Snackbar.make(
+                requireContext(),
+                binding.root,
+                binding.comments.commentInput.text.toString(),
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
+
     }
 
     override fun onDestroyView() {
@@ -90,5 +129,7 @@ class CardFragment : Fragment() {
 
     companion object {
         const val CARD_CARD_KEY = "CARD"
+        const val DEGREE_OF_ROTATION = 180f // Угол поворота 180
+        const val NOT_ROTATION = 0f
     }
 }
